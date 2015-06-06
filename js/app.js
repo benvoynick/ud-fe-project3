fa1ed0aa0f8baa4749e57a438e71b684efe25193
Ben Voynick <ben.voynick@gmail.com>
2015-06-06 14:21:37 -0400

    Move stage rendering code to AStage class, first step in implementing different stages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var AGameState = function() {
     this.level = 1;
+    
+    this.stage = new AStage();
 }
 
 AGameState.prototype.renderLevel = function() {
@@ -56,6 +58,50 @@ AGameState.prototype.win = function() {
     this.lose();
 }
 
+
+
+var AStage = function() {
+    
+}
+
+AStage.prototype.render = function() {
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    
+    /* This array holds the relative URL to the image used
+     * for that particular row of the game level.
+     */
+    var rowImages = [
+            'images/water-block.png',   // Top row is water
+            'images/stone-block.png',   // Row 1 of 3 of stone
+            'images/stone-block.png',   // Row 2 of 3 of stone
+            'images/stone-block.png',   // Row 3 of 3 of stone
+            'images/grass-block.png',   // Row 1 of 2 of grass
+            'images/grass-block.png'    // Row 2 of 2 of grass
+        ],
+        numRows = 6,
+        numCols = 5,
+        row, col;
+
+    /* Loop through the number of rows and columns we've defined above
+     * and, using the rowImages array, draw the correct image for that
+     * portion of the "grid"
+     */
+    for (row = 0; row < numRows; row++) {
+        for (col = 0; col < numCols; col++) {
+            /* The drawImage function of the canvas' context element
+             * requires 3 parameters: the image to draw, the x coordinate
+             * to start drawing and the y coordinate to start drawing.
+             * We're using our Resources helpers to refer to our images
+             * so that we get the benefits of caching these images, since
+             * we're using them over and over.
+             */
+            ctx.drawImage(Resources.get(rowImages[row]), col * colWidth, row * rowHeight);
+        }
+    }
+}
 
 
 var Mortal = function() {
