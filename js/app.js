930261ec3f2b25bb08387c3384020a3abfe08500
Ben Voynick <ben.voynick@gmail.com>
2015-06-05 23:49:10 -0400

    Add empty allEnemies array to get engine rendering

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ APlayer.prototype.handleInput = function(keyPressed) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 player = new APlayer();
+allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
